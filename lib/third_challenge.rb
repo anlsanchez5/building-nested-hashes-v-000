d268e9d403e3410ae7ed534bec11d591c381b72e
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-09-15 00:30:54 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,7 +11,7 @@ def third_challenge
         },
       :hero => {
         :name => "Romeo",
-        :age => "15"
+        :age => "15",
         :status => "alive"
         },
       :hero_friends => []
@@ -26,8 +26,8 @@ def third_challenge
         :age => "51"
         },
       :heroine => {
-        :name => "Juliet"
-        :age => "15"
+        :name => "Juliet",
+        :age => "15",
         :status => "alive"
         },
       :heroine_friends => []
